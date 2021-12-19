6094fc57a77c6a8098a1c39cffc0ca3dd609ed07
desmondchiam18 <desmondchiam18@gmail.com>
2021-12-19 04:28:27 +0000

    done exercise 2

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    let name = ""
+    let age = ""
+
+    async function getName() {
+    fetch('https://api.agify.io/?name=' + name)
+      .then((response) => response.json())
+      .then((data) => {
+        age = data.age; 
+      });
+    }
+</script>
+
+
+<h1>Guess Age</h1>
+
+<form on:submit|preventDefault={getName}>
+    <label for="name">Name:</label>
+    <input id="name" bind:value={name} type="text" />
+    <button type="submit">Check</button>
+</form>
+<p>Age: {age}</p>
